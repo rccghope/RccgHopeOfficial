4788eb7c0ade62d3c5d52b0917bda1a0afe5a30e
Emzzy241 <emzzyoluwole@gmail.com>
2025-04-13 01:58:41 +0100

    completed the about-us 6th section

--- frontend/src/Pages/AboutUs.tsx
+++ frontend/src/Pages/AboutUs.tsx
@@ -7,6 +7,7 @@ import AboutSection2Skeleton from '../LazyLoad/AboutSection2Skeleton';
 import AboutSection3Skeleton from '../LazyLoad/AboutSection3Skeleton';
 import AboutSection4Skeleton from '../LazyLoad/AboutSection4Skeleton';
 import AboutSection5Skeleton from '../LazyLoad/AboutSection5Skeleton';
+import AboutSection6Skeleton from '../LazyLoad/AboutSection6Skeleton';
 
 const AboutBanner = React.lazy(() => import('../components/AboutBanner'));
 const AboutSection1 = React.lazy(() => import('../components/AboutSection1'));
@@ -14,6 +15,7 @@ const AboutSection2 = React.lazy(() => import('../components/AboutSection2'));
 const AboutSection3 = React.lazy(() => import('../components/AboutSection3'));
 const AboutSection4 = React.lazy(() => import('../components/AboutSection4'));
 const AboutSection5 = React.lazy(() => import('../components/AboutSection5'));
+const AboutSection6 = React.lazy(() => import('../components/AboutSection6'));
 
 const AboutUs = () => {
   return (
@@ -66,6 +68,15 @@ const AboutUs = () => {
           {' '}
           <Suspense fallback={<AboutSection5Skeleton />}>
             <AboutSection5 />
+          </Suspense>
+        </ErrorBoundary>
+      </div>
+      
+      <div className="">
+        <ErrorBoundary>
+          {' '}
+          <Suspense fallback={<AboutSection6Skeleton />}>
+            <AboutSection6 />
           </Suspense>
         </ErrorBoundary>
       </div>
